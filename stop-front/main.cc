6de9846a1284f226489f5c8f7001c1d652f6f158
sebastian <sebastian@skjoenberg.com>
2015-09-01 16:16:43 +0200

    nu stikker det af

--- stop-front/main.cc
+++ stop-front/main.cc
@@ -5,12 +5,18 @@
 using namespace PlayerCc;
 
 int main(int argc, char** argv) {
+    // Distance
     double mindistance = 0.70;
+
+    // Objekter
     PlayerClient    robot(gHostname, gPort);
     Position2dProxy pp(&robot, gIndex);
     IrProxy ir(&robot, gIndex);
 
+    // Kør ligeud
     pp.SetSpeed(0.1, 0.0);
+
+    // LÆS LASER!
     while (1) {
         robot.Read();
         if (ir.GetRange(SCORPION_IR_BN_N) < mindistance){
@@ -19,5 +25,5 @@ int main(int argc, char** argv) {
         }
         sleep(0.2);
     }
-    std::cout << "Hej" << std::endl;
+    std::cout << "Jeg er færdig mester!" << std::endl;
 }
