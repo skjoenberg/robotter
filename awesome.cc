ac0d51d248a53552f1a8d60823d069bc3eec56d4
sebastian <sebastian@skjoenberg.com>
2015-09-11 10:47:08 +0200

    små turns

--- awesome.cc
+++ awesome.cc
@@ -5,16 +5,17 @@
 #include <scorpion.h>
 using namespace PlayerCc;
 
-#define MIN_DISTANCE 0.70
+#define MIN_DISTANCE 0.50
+#define SPEED 0.2
 
 void back_the_fuck_up(Position2dProxy* pp, PlayerClient* robert) {
-    pp->SetSpeed(-0.3, 0.0);
+    pp->SetSpeed(-SPEED, 0.0);
     sleep(2);
     pp->SetSpeed(0.0, 0.0);
 }
 
 void go_bot_go(Position2dProxy* pp) {
-    pp->SetSpeed(0.3, 0.0);
+    pp->SetSpeed(SPEED, 0.0);
 }
 
 bool obsFront(IrProxy* ir) {
@@ -24,21 +25,27 @@ bool obsFront(IrProxy* ir) {
 }
 
 bool obsLeft(IrProxy* ir) {
-    return (ir->GetRange(SCORPION_IR_BN_NW) < MIN_DISTANCE ||
-            ir->GetRange(SCORPION_IR_TW_NNW));
+    return (ir->GetRange(SCORPION_IR_BN_NW) < (MIN_DISTANCE-0.1) ||
+            ir->GetRange(SCORPION_IR_TW_NNW) < (MIN_DISTANCE-0.1));
 }
 
 bool obsRight(IrProxy* ir) {
-    return (ir->GetRange(SCORPION_IR_BN_NE) < MIN_DISTANCE ||
-            ir->GetRange(SCORPION_IR_TE_NNE));
+    return (ir->GetRange(SCORPION_IR_BN_NE) < (MIN_DISTANCE-0.1) ||
+            ir->GetRange(SCORPION_IR_TE_NNE) < (MIN_DISTANCE-0.1));
 }
 
 void turnRight(Position2dProxy* pp, IrProxy* ir, PlayerClient* robert) {
     pp->SetSpeed(0.0, -0.5);
+    while (obsFront(ir)) {
+        robert->Read();
+    }
 }
 
 void turnLeft(Position2dProxy* pp, IrProxy* ir, PlayerClient* robert) {
     pp->SetSpeed(0.0, 0.5);
+    while (obsFront(ir)) {
+        robert->Read();
+    }
 }
 
 void frontAction(Position2dProxy* pp, IrProxy* ir, PlayerClient* robert) {
@@ -66,15 +73,22 @@ int main(int argc, char** argv) {
         if (bumper.IsAnyBumped()) {
             back_the_fuck_up(&pp, &robert);
         }
+        // Check front
+        else if (obsFront(&ir)) {
+            if (obsRight(&ir)) {
+                turnLeft(&pp, &ir, &robert);
+            }
+            else {
+                turnRight(&pp, &ir, &robert);
+            }
+        }
         else if (obsLeft(&ir)){
             turnRight(&pp, &ir, &robert);
         }
         else if (obsRight(&ir)) {
             turnLeft(&pp, &ir, &robert);
         }
-        else if (obsFront(&ir)) {
-            turnLeft(&pp, &ir, &robert);
-        } else {
+        else {
             go_bot_go(&pp);
         }
     }
