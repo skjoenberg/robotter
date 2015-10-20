e0dbdde0a348cad54c1c9b360b6ca6d19dd05c67
Burger <wrz641@ku.dk>
2015-10-20 14:49:20 +0200

    den er ikke i vinkel

--- Exercise5/exercise5.cc
+++ Exercise5/exercise5.cc
@@ -131,7 +131,7 @@ int main()
   cvMoveWindow (window, 20, 20);
 
   // Initialize particles
-  const int num_particles = 2000;
+  const int num_particles = 10;
   std::vector<particle> particles(num_particles);
   for (int i = 0; i < num_particles; i++)
     {
@@ -276,18 +276,37 @@ int main()
           /* Vægten er givet ved den funktion der står opgaven */
 
 
-          add_uncertainty(particles, 10, 0);
+          //add_uncertainty(particles, 10, 1);
 
           double tmpweight;
           double sum = 0;
           double dist;
           double gaussman = 1. / sqrt(2. * M_PI * pow(SIGMA, 2.));
+          double angletobox;
+          double deltaangle;
+          double distweight;
+          double angleweight;
           for (int i = 0; i < particles.size(); i++) {
               // Measure euclidean distance to landmark
               dist = sqrt(pow(particles[i].x - box_x, 2.0) + pow(particles[i].y - box_y, 2.0));
-
+              angletobox = atan((particles[i].y - box_y)/(particles[i].x - box_x));
+              deltaangle = particles[i].theta - angletobox;
+              if(deltaangle > 2*M_PI){
+                deltaangle - 2*M_PI;
+              }
+              if(deltaangle < -2*M_PI){
+                deltaangle + 2*M_PI;
+              }
               // Calculate weight of the current particle
-              tmpweight = gaussman * exp(-((pow(measured_distance - dist, 2.0) / (2.0 * pow(SIGMA, 2.0)))));
+              angleweight = gaussman * exp(-((pow(measured_angle - deltaangle, 2.0) / (2.0 * pow(SIGMA, 2.0)))));
+              distweight = gaussman * exp(-((pow(measured_distance - dist, 2.0) / (2.0 * pow(SIGMA, 2.0)))));
+              cout << "angleweight: " <<  angleweight << endl;              
+              cout << "deltaangle: " <<  deltaangle << endl;              
+              cout << "angletobox: " <<  angletobox << endl;              
+              cout << "particles theta: " <<  particles[i].theta << endl;              
+              cout << "particles x: " <<  particles[i].x << endl;              
+              cout << "particles y: " <<  particles[i].y << endl << endl;              
+              tmpweight = angleweight*distweight;
 
               // Add the weight to a sum (used later on to normalize weights)
               sum += tmpweight;
