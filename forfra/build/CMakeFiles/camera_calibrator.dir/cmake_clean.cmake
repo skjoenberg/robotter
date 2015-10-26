137d033b13f52bdabc5285997b61d8c26bb62e80
sebastian <sebastian@skjoenberg.com>
2015-10-26 23:11:30 +0100

    nu kører vi

---
+++ forfra/build/CMakeFiles/camera_calibrator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/camera_calibrator.dir/camera_calibrator.cc.o"
+  "camera_calibrator.pdb"
+  "camera_calibrator"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/camera_calibrator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
