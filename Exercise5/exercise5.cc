48b3c08537b7e6fc94297eb8a95c9488ed386663
Bubris <nzl128@alumni.ku.dk>
2015-10-27 13:50:20 +0100

    flyttet på noget

--- Exercise5/exercise5.cc
+++ Exercise5/exercise5.cc
@@ -184,8 +184,16 @@ int main()
 
                 robert.read();
                 double theta_before = robert.pp->GetYaw();
-
                 //                robert.turnXradians(0.17);
+
+                // Move particles (only angle)
+                robert.read();
+                double deltatheta = robert.pp->GetYaw() - theta_before;
+                for(int i = 0; i < particles.size(); i++) {
+                    move_particle(particles[i], 0, 0, deltatheta);
+                }
+                add_uncertainty(particles, 5, 0.1);
+
                 timespec hej = {0, 500000};
                 nanosleep(&hej, NULL);
                 IplImage *im = cam.get_colour();
@@ -327,16 +335,11 @@ int main()
                     for (int i = 0; i < num_particles; i++) {
                         particles[i].weight = 1.0/(double)num_particles;
                     }
-                }
+                } // end: if (not found_landmark)
 
-                robert.read();
-                double deltatheta = robert.pp->GetYaw() - theta_before;
 
-                // Move particles (only angle)
-                for(int i = 0; i < particles.size(); i++) {
-                    move_particle(particles[i], 0, 0, deltatheta);
-                }
-                add_uncertainty(particles, 5, 0.1);
+
+
 
                 ////////////////
                 // Draw stuff //
@@ -351,45 +354,45 @@ int main()
                 draw_world (est_pose, particles, world);
                 cvShowImage (map, world);
                 cvShowImage (window, im);
-            }  // end: if (not found_landmark)
+            } // end search mode inner loop (cirkel)
             search_mode = false;
             cout << "Finished searching." << endl;
         } else {
-        cout << "saa rykker vi!" << endl;
-        int target_x, target_y, deltax, deltay, dist, angletotarget, deltaangle;
-        target_x = 150;
-        target_y = 0;
-        deltax = est_pose.x - target_x;
-        deltay = est_pose.y - target_y;
+            cout << "saa rykker vi!" << endl;
+            int target_x, target_y, deltax, deltay, dist, angletotarget, deltaangle;
+            target_x = 150;
+            target_y = 0;
+            deltax = est_pose.x - target_x;
+            deltay = est_pose.y - target_y;
 
-        // Euclidean distance to box
-        dist = sqrt(pow(deltax, 2.0) + pow(deltay, 2.0));
+            // Euclidean distance to box
+            dist = sqrt(pow(deltax, 2.0) + pow(deltay, 2.0));
 
-        // Angle between particle and box
-        angletotarget = atan(deltay / deltax);
+            // Angle between particle and box
+            angletotarget = atan(deltay / deltax);
 
-        // If deltax > 0, then the angle needs to be turned by a half circle.
-        if (deltax > 0) {
-            angletotarget -= M_PI;
+            // If deltax > 0, then the angle needs to be turned by a half circle.
+            if (deltax > 0) {
+                angletotarget -= M_PI;
+            }
+
+            // Difference in angle
+            deltaangle = est_pose.theta - angletotarget;
+
+            // The angles are between (-pi, pi)
+            if (deltaangle > M_PI){
+                deltaangle -= 2 * M_PI;
+            } else if (deltaangle < -M_PI) {
+                deltaangle += 2 * M_PI;
+            }
+            cout << deltaangle << endl;
+            cout << dist << endl;
+            robert.turnXradians(-deltaangle);
+            robert.moveXcm(dist);
+            cout << "slut" << endl;
+            while (true) {
+            }
         }
-
-        // Difference in angle
-        deltaangle = est_pose.theta - angletotarget;
-
-        // The angles are between (-pi, pi)
-        if (deltaangle > M_PI){
-            deltaangle -= 2 * M_PI;
-        } else if (deltaangle < -M_PI) {
-            deltaangle += 2 * M_PI;
-        }
-        cout << deltaangle << endl;
-        cout << dist << endl;
-        robert.turnXradians(-deltaangle);
-        robert.moveXcm(dist);
-        cout << "slut" << endl;
-        while (true) {
-        }
-    }
 
         // add_uncertainty(particles, 10, 0.2);
         // robert.read();
