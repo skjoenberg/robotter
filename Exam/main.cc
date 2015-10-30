b346668157dc12e5dd31e4e64e869fe66a65d08d
Bubris <nzl128@alumni.ku.dk>
2015-10-30 13:40:00 +0100

    røv

--- Exam/main.cc
+++ Exam/main.cc
@@ -176,6 +176,7 @@ int main()
 
             robert.read();
             delta_theta = robert.pp->GetYaw() - theta_before;
+            cout << "VI FLYTTER PARTIKLERNE MED " << delta_theta << endl;
             for(int i = 0; i < particles.size(); i++) {
                 move_particle(particles[i], 0, 0, delta_theta);
             }
