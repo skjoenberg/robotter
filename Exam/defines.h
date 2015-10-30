c48f5cd0a5aa09ea8c9fddf04e51f1c0ed447e53
Burger <wrz641@ku.dk>
2015-10-30 09:41:23 +0100

    se alle de commits

--- Exam/defines.h
+++ Exam/defines.h
@@ -8,13 +8,5 @@
 #define SIGMA_THETA 0.1
 
 
-const CvPoint landmarks [NUM_LANDMARKS] = {
-    cvPoint (0, 300),
-    cvPoint (0, 0),
-    cvPoint (400, 300),
-    cvPoint (400, 0)
-};
-
- 
 
 #define NUM_LANDMARKS 4
