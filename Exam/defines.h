448ea4c98f16f4dd4d72183e31e717b550d11831
Burger <wrz641@ku.dk>
2015-10-30 09:40:51 +0100

    fix pls

--- Exam/defines.h
+++ Exam/defines.h
@@ -7,7 +7,14 @@
 #define SIGMA 20.0
 #define SIGMA_THETA 0.1
 
-#define TARGET_X 150
-#define TARGET_Y 0
+
+const CvPoint landmarks [NUM_LANDMARKS] = {
+    cvPoint (0, 300),
+    cvPoint (0, 0),
+    cvPoint (400, 300),
+    cvPoint (400, 0)
+};
+
+ 
 
 #define NUM_LANDMARKS 4
